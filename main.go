2aa052dff7e9b09f4aa919532d0e291606e49b79
Adrian <adpenagosa@gmail.com>
2026-03-02 20:26:11 -0600

    add: go sum con el sqlite

--- main.go
+++ main.go
@@ -30,7 +30,46 @@ func get(conn net.Conn, db *sql.DB) {
 	}
 
 	if method == "GET" && path == "/" {
-		//tabla principal
+		
+		var id int
+		var name string
+		var current int
+		var total int
+
+		html := `
+		<html>
+		<head>
+		<title>My Series Tracker</title>
+		</head>
+		<body>
+
+		<h1>My Series Tracker</h1>
+
+		<table border="1" cellpadding="8" cellspacing="0">
+		<tr>
+		<th>#</th>
+		<th>Name</th>
+		<th>Current</th>
+		<th>Total</th>
+		</tr>
+		`
+
+		for rows.Next() {
+			rows.Scan(&id, &name, &current, &total)
+
+			html += fmt.Sprintf(
+				"<tr><td>%d</td><td>%s</td><td>%d</td><td>%d</td></tr>",
+				id, name, current, total,
+			)
+		}
+
+		html += "</table></body></html>"
+
+		response := "HTTP/1.1 200 OK\r\n" +
+			"Content-Type: text/html\r\n" +
+			fmt.Sprintf("Content-Length: %d\r\n\r\n%s", len(html), html)
+
+		conn.Write([]byte(response))
 	}
 
 	if method == "GET" && path == "/create" {
@@ -48,45 +87,7 @@ func get(conn net.Conn, db *sql.DB) {
 	rows, _ := db.Query("SELECT id, name, current_episode, total_episodes FROM series")
 	defer rows.Close()
 
-	var id int
-	var name string
-	var current int
-	var total int
 
-	html := `
-	<html>
-	<head>
-	<title>My Series Tracker</title>
-	</head>
-	<body>
-
-	<h1>My Series Tracker</h1>
-
-	<table border="1" cellpadding="8" cellspacing="0">
-	<tr>
-	<th>#</th>
-	<th>Name</th>
-	<th>Current</th>
-	<th>Total</th>
-	</tr>
-	`
-
-	for rows.Next() {
-		rows.Scan(&id, &name, &current, &total)
-
-		html += fmt.Sprintf(
-			"<tr><td>%d</td><td>%s</td><td>%d</td><td>%d</td></tr>",
-			id, name, current, total,
-		)
-	}
-
-	html += "</table></body></html>"
-
-	response := "HTTP/1.1 200 OK\r\n" +
-		"Content-Type: text/html\r\n" +
-		fmt.Sprintf("Content-Length: %d\r\n\r\n%s", len(html), html)
-
-	conn.Write([]byte(response))
 }
 
 func main() {
